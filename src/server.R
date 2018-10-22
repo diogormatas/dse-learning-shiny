211eabf00137937e599024cb562f496e894ecf19
Diogo Reis <diogormatas@gmail.com>
2018-10-22 18:00:27 +0100

    Menu subitems created.

--- src/server.R
+++ src/server.R
@@ -9,17 +9,6 @@
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # # generate bins based on input$bins from ui.R
-    # x    <- faithful[, 2]
-    # bins <- seq(min(x), max(x), length.out = input$slider + 1)
-    # 
-    # # draw the histogram with the specified number of bins
-    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    plot(iris$Petal.Length, iris$Petal.Width, pch=21, bg=c("red","green3","blue")[unclass(iris$Species)], main="Edgar Anderson's Iris Data")
-  })
-
   output$progressBox <- renderValueBox({
     valueBox(
       paste0(25 + input$slider, "%"), "Progress", icon = icon("list"),
@@ -34,10 +23,54 @@ shinyServer(function(input, output) {
     )
   })
   
-  output$table <- renderDataTable(iris)
+  output$distPlot <- renderPlot({
+    
+    # # generate bins based on input$bins from ui.R
+    # x    <- faithful[, 2]
+    # bins <- seq(min(x), max(x), length.out = input$slider + 1)
+    # 
+    # # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    plot(iris$Petal.Length, iris$Petal.Width, pch=21, bg=c("red","green3","blue")[unclass(iris$Species)], main="Edgar Anderson's Iris Data")
+  })
   
+  output$table <- renderDataTable(iris)
+
   output$sum <- renderPrint({
     
     summary(iris)
   })
+  
+  output$table_preview <- renderDataTable({
+      
+      # input$file1 will be NULL initially. After the user selects
+      # and uploads a file, head of that data file by default,
+      # or all rows if selected, will be shown.
+      
+      req(input$filepath)
+      
+      # when reading semicolon separated files,
+      # having a comma separator causes `read.csv` to error
+      tryCatch(
+        {
+          df <- read.csv(paste0(data_folder,input$filepath),
+                         header = input$header,
+                         sep = input$sep,
+                         quote = input$quote)
+        },
+        error = function(e) {
+          # return a safeError if a parsing error occurs
+          stop(safeError(e))
+        }
+      )
+      return(head(df))
+    })
+    
+  
+  output$file_path_print <- renderPrint({
+    
+    paste0(data_folder,input$filepath)
+  })
+  
+
 })
--- src/ui.R
+++ src/ui.R
@@ -1,4 +1,5 @@
 
+
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -8,102 +9,128 @@
 library(shiny)
 library(shinydashboard)
 
-shinyUI(
-  dashboardPage(skin = "black",
-   dashboardHeader(title = img(src="noesis2.png"),
-                   dropdownMenu(type = "messages",
-                                messageItem(
-                                  from = "Sales Dept",
-                                  message = "Sales are steady this month."
-                                ),
-                                messageItem(
-                                  from = "New User",
-                                  message = "How do I register?",
-                                  icon = icon("question"),
-                                  time = "13:45"
-                                ),
-                                messageItem(
-                                  from = "Support",
-                                  message = "The new server is ready.",
-                                  icon = icon("life-ring"),
-                                  time = "2014-12-01"
-                                )
-                   ),
-                   dropdownMenu(type = "notifications",
-                                notificationItem(
-                                  text = "5 new users today",
-                                  icon("users")
-                                ),
-                                notificationItem(
-                                  text = "12 items delivered",
-                                  icon("truck"),
-                                  status = "success"
-                                ),
-                                notificationItem(
-                                  text = "Server load at 86%",
-                                  icon = icon("exclamation-triangle"),
-                                  status = "warning"
-                                )
-                   ),
-                   
-                   dropdownMenu(type = "tasks", badgeStatus = "success",
-                                taskItem(value = 90, color = "green",
-                                         "Documentation"
-                                ),
-                                taskItem(value = 17, color = "aqua",
-                                         "Project X"
-                                ),
-                                taskItem(value = 75, color = "yellow",
-                                         "Server deployment"
-                                )
-                   )
-                   
-                   ),
-   dashboardSidebar(
-     
-     sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
-                       label = "Search"),
-     sidebarMenu(
-       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"),
-                badgeLabel = "new", badgeColor = "green"),
-       menuItem("Projects", tabName = "projects", icon = icon("suitcase"),
-                badgeLabel = "new", badgeColor = "green"),
-       menuItem("Datasets", tabName = "datasets", icon = icon("table"),
-                badgeLabel = "new", badgeColor = "green"),
-       sliderInput("slider", "Slider input:", 1, 100, 50)
-     )
-   ),
-   dashboardBody(
-     tabItems(
-       tabItem("dashboard","APAGAR -> Cenas do Dashboard",
-     fluidRow(
-       # A static valueBox
-       valueBox(10 * 2, "New Orders", icon = icon("credit-card"),width = 3),
-       
-       # Dynamic valueBoxes
-       valueBoxOutput("progressBox",width = 3),
-       
-       valueBoxOutput("approvalBox",width = 3)
-     ),
-     fluidRow(
-       tabBox(width = 6,
-         id = "tabset1",
-         tabPanel("Chart", 
-                  plotOutput("distPlot")),
-         tabPanel("Table", 
-                  dataTableOutput('table')),
-         tabPanel("Summary",
-                  verbatimTextOutput("sum"))
-         
-       )
-     )
-       ),
-     tabItem("projects","APAGAR -> Cenas do Prjects"),
-     tabItem("datasets","APAGAR -> Cenas do Datasets", 
-             selectInput("v_datasets", "Datasets:",
-                         c(list.files('./data'))))
-     )
-   )
-  )
-)
-
+shinyUI(dashboardPage(
+  skin = "black",
+  dashboardHeader(
+    title = img(src = "noesis2.png"),
+    dropdownMenu(
+      type = "messages",
+      messageItem(from = "Sales Dept",
+                  message = "Sales are steady this month."),
+      messageItem(
+        from = "New User",
+        message = "How do I register?",
+        icon = icon("question"),
+        time = "13:45"
+      ),
+      messageItem(
+        from = "Support",
+        message = "The new server is ready.",
+        icon = icon("life-ring"),
+        time = "2014-12-01"
+      )
+    ),
+    dropdownMenu(
+      type = "notifications",
+      notificationItem(text = "5 new users today",
+                       icon("users")),
+      notificationItem(text = "12 items delivered",
+                       icon("truck"),
+                       status = "success"),
+      notificationItem(
+        text = "Server load at 86%",
+        icon = icon("exclamation-triangle"),
+        status = "warning"
+      )
+    ),
+    
+    dropdownMenu(
+      type = "tasks",
+      taskItem(value = 90, color = "green",
+               "Documentation"),
+      taskItem(value = 17, color = "aqua",
+               "Project X"),
+      taskItem(value = 75, color = "yellow",
+               "Server deployment")
+    )
+    
+  ),
+  dashboardSidebar(
+    sidebarSearchForm(
+      textId = "searchText",
+      buttonId = "searchButton",
+      label = "Search"
+    ),
+    sidebarMenu(
+      menuItem(
+        "Dashboard",
+        tabName = "dashboard",
+        icon = icon("dashboard")
+      ),
+      menuItem("Exploratory", tabName = "exploratory", icon = icon("searchengin"), startExpanded = FALSE,
+        menuSubItem("Datasets", tabName = "datasets", icon = icon("table")),
+        menuSubItem("Analytics", tabName = "analytics", icon = icon("braille"))
+      ),
+      sliderInput("slider", "Slider input:", 1, 100, 50)
+    )
+  ),
+  dashboardBody(tabItems(
+    tabItem(
+      "dashboard",
+      fluidRow(
+        # A static valueBox
+        valueBox(
+          10 * 2,
+          "New Orders",
+          icon = icon("credit-card"),
+          width = 3
+        ),
+        
+        # Dynamic valueBoxes
+        valueBoxOutput("progressBox", width = 3),
+        
+        valueBoxOutput("approvalBox", width = 3)
+      ),
+      fluidRow(tabBox(
+        width = 6,
+        id = "tabset1",
+        tabPanel("Chart",
+                 plotOutput("distPlot")),
+        tabPanel("Table",
+                 dataTableOutput('table')),
+        tabPanel("Summary",
+                 verbatimTextOutput("sum"))
+        
+      ))
+    ),
+    tabItem(
+      "datasets",
+      fluidRow(
+        
+      selectInput("filepath", "Datasets:",
+                  c(list.files(data_folder))),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"')),
+      
+      
+      "Preview:",
+      dataTableOutput("table_preview")
+      
+    )
+  ))
+))
